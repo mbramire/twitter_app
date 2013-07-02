501ce400fa3835542eaab5c3834bd20243fe6f58
Matt Ramirez <mbramire@live.com>
2013-07-02 15:39:16 -0400

    pulled to mac, edited envirnoment

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count) 
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_content('error') }
+        it { should have_selector('title', text: 'Sign up') }
+      end
     end
 
     describe "with valid data" do
