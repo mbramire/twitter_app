e6da93e41b2be338af621f574f3617378bbe828f
Matt Ramirez <mbramire@live.com>
2013-07-03 14:27:14 -0400

    chapter 8 completed

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -50,10 +50,15 @@ describe "User pages" do
 
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "followed by sign out" do
+        it { should have_link('Sign in') }
       end
     end
   end
