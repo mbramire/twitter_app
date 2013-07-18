55ba4b745e17e3a49227158fa147adc0f3b3cc12
Matt Ramirez <mbramire@live.com>
2013-07-18 15:00:59 -0400

    completed chapter 11

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -38,6 +38,7 @@ describe "Authentication" do
 
   describe "Authorization" do
   	describe "for non-signed in users" do
+      let(:user) { FactoryGirl.create(:user) }
 
   		describe "when attempting to visit a protected page" do
         before do
@@ -109,6 +110,18 @@ describe "Authentication" do
           it { should have_selector('title', text: 'Sign in') }
         end
   		end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
   	end
 
   	describe "as the wrong user" do
